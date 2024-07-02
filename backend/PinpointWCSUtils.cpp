dee70a387e6fcbf7cb1208ffef73ce58852c64dc
Peter Williams <peter@newton.cx>
2024-07-02 10:39:05 -0400

    backend/PinpointWCSUtils.cpp: homogenize whitespace

--- backend/PinpointWCSUtils.cpp
+++ backend/PinpointWCSUtils.cpp
@@ -28,7 +28,7 @@
 #include "PinpointWCSUtils.h"
 
 namespace PinpointWCSUtils {
-	
+
 	float computeQuantile(float *arr, long arraysize, double q)
 	{
 //		std::cout << "Determining Quantile ...\n";
@@ -42,26 +42,26 @@ namespace PinpointWCSUtils {
 		float* s1;
 		float* s2;
 		float eps = 1. / length;
-		
+
 		// Initialize a random number
 		srand(time(NULL));
-		
+
 		// Copy data to local variable
 		D = (float *) malloc(length * sizeof(float));
 		memcpy(D, arr, length * sizeof(float));
-		
+
 		while (true) {
-			
+
 			// Choose element from array
 			randIndex = rand() % length;
 			dd = D[randIndex];
 			s1len = 0;
 			s2len = 0;
-			
+
 			// Allot memory to the two arrays created
 			s1 = (float *) malloc((length) * sizeof(float));
 			s2 = (float *) malloc((length) * sizeof(float));
-			
+
 			// Begin comparison
 			for (i=0; i<length; i++)
 			{
@@ -76,18 +76,18 @@ namespace PinpointWCSUtils {
 					s2len++;
 				}
 			}
-			
+
 			// Reallocate memory to both arrays
 			s1 = (float *) realloc(s1, s1len * sizeof(float));
 			s2 = (float *) realloc(s2, s2len * sizeof(float));
-			
+
 			// Check length of s1
 			if (s1len == 0)
 				return dd;
-			
+
 			// Determine the quantile of d
 			dq = (double) (s1len + num_below) / arraysize;
-			
+
 			// Choose the appropriate array
 			if (dq > q - eps and dq < q + eps)
 				return dd;
@@ -104,7 +104,7 @@ namespace PinpointWCSUtils {
 				// Choose s1
 				D = (float *) realloc(D, s1len * sizeof(float));
 				memcpy(D, s1, s1len * sizeof(float));
-				length = s1len;				
+				length = s1len;
 			}
 
 			// Deallocate s1 and s2
@@ -115,75 +115,75 @@ namespace PinpointWCSUtils {
 //			std::cout << "length = " << length << "\n";
 //			std::cout << "dq = " << dq << "\n";
 //			std::cout << "iter = " << iter << "\n\n";
-			
+
 			iter++;
 		}  // while
-		
+
 	} // computeQuantile function
-	
-	
+
+
 	bool cen3(float f0, float f1, float f2, float *xcen)
 	{
 		float a, b;
-		
+
 		a = 0.5 * (f2 - 2*f1 + f0);
 		if (a == 0.0)
 			return false;
-		
+
 		b = f1 - a - f0;
 		*xcen = -0.5 * b / a;
 		if ((*xcen < 0.0) || (*xcen > 2.0))
 			return false;
-		
+
 		return true;
 	} // cen3 function
-	
-	
+
+
 	bool cen3x3(float *image, float *xcen, float *ycen)
 	{
 		float mx0, mx1, mx2;
 		float my0, my1, my2;
 		float bx, by, mx , my;
 		int badcen = 0;
-		
+
 		badcen += cen3(image[0 + 3*0], image[1 + 3*0], image[2 + 3*0], &mx0);
 		badcen += cen3(image[0 + 3*1], image[1 + 3*1], image[2 + 3*1], &mx1);
 		badcen += cen3(image[0 + 3*2], image[1 + 3*2], image[2 + 3*2], &mx2);
-		
+
 		badcen += cen3(image[0 + 3*0], image[0 + 3*1], image[0 + 3*2], &my0);
 		badcen += cen3(image[1 + 3*0], image[1 + 3*1], image[1 + 3*2], &my1);
 		badcen += cen3(image[2 + 3*0], image[2 + 3*1], image[2 + 3*2], &my2);
-		
+
 		/* are we not okay? */
 		if (badcen != 6) {
 			return false;
 		}
-		
+
 		/* x = (y-1) mx + bx */
 		bx = (mx0 + mx1 + mx2) / 3.;
 		mx = (mx2 - mx0) / 2.;
-		
+
 		/* y = (x-1) my + by */
 		by = (my0 + my1 + my2) / 3.;
 		my = (my2 - my0) / 2.;
-		
+
 		/* find intersection */
 		(*xcen) = (mx * (by - my - 1.) + bx) / (1. + mx * my);
 		(*ycen) = ((*xcen) - 1.) * my + by;
-		
+
 		printf("%f\n", *xcen);
 		printf("%f\n", *ycen);
-		
+
 		/* check that we are in the box */
 		if (((*xcen) < 0.0) || ((*xcen) > 2.0) ||
 			((*ycen) < 0.0) || ((*ycen) > 2.0)){
 			return false;
 		}
-		
+
         /* check for nan's and inf's */
 		if (!isnormal(*xcen) || !isnormal(*ycen))
 			return false;
-		
+
 		return true;
 	} // cen3x3
 
@@ -194,7 +194,7 @@ namespace PinpointWCSUtils {
 		std::cout << "CRVAL2:\t" << wcs->yref << std::endl;
 		std::cout << "CRPIX1:\t" << wcs->xrefpix << std::endl;
 		std::cout << "CRPIX2:\t" << wcs->yrefpix << std::endl;
-		
+
 		std::cout << "CDELT1:\t" << wcs->xinc << std::endl;
 		std::cout << "CDELT2:\t" << wcs->yinc << std::endl;
 		std::cout << "CROTA2:\t" << wcs->rot << std::endl;
@@ -203,19 +203,19 @@ namespace PinpointWCSUtils {
 		std::cout << "CD2_1:\t" << wcs->cd[2] << std::endl;
 		std::cout << "CD2_2:\t" << wcs->cd[3] << std::endl;
 		std::cout << "EPOCH:\t" << wcs->equinox << std::endl;
-		
+
 		std::cout << "DATE-OBS:\t" << wcs->epoch << std::endl;
 		std::cout << "NAXIS1:\t" << wcs->nxpix << std::endl;
 		std::cout << "NAXIS2:\t" << wcs->nypix << std::endl;
 		std::cout << "NAXIS:\t" << wcs->naxes << std::endl;
 		std::cout << "CTYPE1:\t" << wcs->c1type << std::endl;
-		
+
 		std::cout << "CTYPE2:\t" << wcs->c2type << std::endl;
 		std::cout << "CUNITn:\t" << wcs->units << std::endl;
 		std::cout << "RADECSYS:\t" << wcs->radecsys << std::endl;
 		std::cout << " ---------- ---------- ---------- ---------- ---------- ---------- ----------" << std::endl;
-		
+
 	} // dumpWCS
-	
+
 }  // namespace
 
